b99c6ad271435f5046bc2821303c6ddfe0b5e9e4
Thomaz Peres <thomaz.peres21@hotmail.com>
2020-10-12 21:46:42 -0300

    Começando o projeto, continuo algum dia

---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    /*
+    tutotial https://unitytutoriais.wordpress.com/2016/02/08/visao-em-primeira-pessoa-no-unity/
+    float mouseX = Input.GetAxis("Mouse X");
+    float mouseY = Input.GetAxis("Mouse Y");
+    float sensitivity = 60f;
+
+    float movementY = mouseY * sensitivity * Time.deltaTime;
+
+    float newAngleY = transform.eulerAngles.x + movementY;
+    transform.Rotate(0f, mouseX* sensitivity * Time.deltaTime, 0f, Space.World);
+
+        if (newAngleY <= 20 || newAngleY >= 270)
+        {
+            transform.Rotate(movementY, 0f, 0f);
+        }
+    */
+
+    [SerializeField] private float cameraSensitivity = 100f;
+    [SerializeField] private float minimumAngle = -60f, maximumAngle = 60f;
+    [SerializeField] private float rotation = 0f;
+    public Transform transformPlayer;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+    
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * cameraSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * cameraSensitivity * Time.deltaTime;
+
+        rotation -= mouseY;
+        rotation = Mathf.Clamp(rotation, minimumAngle, maximumAngle);
+
+        transform.localRotation = Quaternion.Euler(rotation, 0, 0);
+
+        transformPlayer.Rotate(Vector3.up * mouseX);
+    }
+}
---
+++ Assets/Scripts/Player/Movimentation.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movimentation : MonoBehaviour
+{
+    [SerializeField] private float speed, speedRotation;
+
+    void Update()
+    {
+        float movimentacao = (Input.GetAxis("Vertical") * speed) * Time.deltaTime;
+        float rotation = (Input.GetAxis("Horizontal") * speedRotation) * Time.deltaTime;
+
+        // transform.Rotate(0, rotation, 0);
+        transform.Translate(0, 0 , movimentacao);
+        transform.Translate(rotation, 0, 0);
+    }
+}
